5b2a12128947329b0a45b513f5a419059597cdac
Nagulesh N <nagulesh.22cs@kct.ac.in>
2023-12-26 15:43:25 +0530

    objects created

--- event/models.py
+++ event/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 mode_choice=(('online','online'),('offline','offline'))
+varieties=(('technical','technical'),('non-technical','non-technical'))
 class Event(models.Model):
     event_name=models.CharField( max_length=20)
     instruction=models.CharField(max_length=1000)
-    category=models.ForeignKey(varieties,on_delete=models.CASCADE)
+    category=models.CharField(max_length=14,choices=varieties)
     date=models.DateField()
     mode=models.CharField( max_length=10,  choices=mode_choice)
     registration_fee=models.IntegerField()
@@ -17,6 +18,6 @@ class Event(models.Model):
     def __str__(self):
         return self.event_name
     
-class registeration(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    event=models.CharField(max_length=30)
+# class registeration(models.Model):
+#     user=models.ForeignKey(User,on_delete=models.CASCADE)
+#     event=models.CharField(max_length=30)
--- event/views.py
+++ event/views.py
@@ -7,4 +7,17 @@ def login(request):
   return render(request, 'credential/login.html')
 
 def add_event(request):
+  if request.method =="POST":
+    event_name=request.POST.get("event_name")
+    instruction=request.POST.get("description")
+    category=request.POST.get("category")
+    date=request.POST.get("date")
+    mode=request.POST.get("mode")
+    registration_fee=request.POST.get("registration_fee")
+    prize_amount=request.POST.get("prize")
+    contact_number=request.POST.get("contact")
+    time=request.POST.get("time")
+    venue=request.POST.get("venue")
+    Event.objects.create(event_name=event_name,instruction=instruction,category=category,date=date,mode=mode,registration_fee=registration_fee,prize_amount=prize_amount,contact_number=contact_number,duration=duration,venue=venue)
+    
   return render(request, 'core/add_event.html')
