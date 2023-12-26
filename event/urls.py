10f9a8d0427348216d6f99549f89c81a36d2cf50
Chaaivisva <126050738+Chaaivisva@users.noreply.github.com>
2023-12-26 12:37:41 +0530

    Home template added.

--- event/urls.py
+++ event/urls.py
@@ -1,1 +1,12 @@
 from django.urls import path 
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+  path('home/', views.home, name="Home"),
+  path('', views.login, name="Login"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
