fd98942d13448f08f4f9164d70601378ccda3e93
Nagulesh N <nagulesh.22cs@kct.ac.in>
2023-12-31 18:25:48 +0530

    login functionality updated

--- event/urls.py
+++ event/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-  path('', views.login, name="Login"),
-  path('home/', views.home, name="Home"),
+  path('login/', views.login, name="Login"),
+  path('', views.home, name="Home"),
   path('event/<int:no>', views.event_description, name="event"),
   path('add_event/', views.add_event, name="Add_event"),
 ]
--- event/views.py
+++ event/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User,auth
+from django.shortcuts import render ,redirect
 from . models import *
 
+
+def login(request):
+  if request.method =="POST":
+      print("hello")
+      username=request.POST.get("username")
+      password=request.POST.get("password")
+      print(username,password)
+      user=auth.authenticate(username=username,password=password)
+      print(user)
+      if user is not None:
+          auth.login(request,user)
+          return render(request, 'core/home.html')
+      else:
+          return redirect('Login')
+  return render(request, 'credential/login.html')
+
+@login_required(login_url='/login/')
 def home(request):
   events=Event.objects.all()
   return render(request, 'core/home.html',{'events':events})
 
-def login(request):
-  return render(request, 'credential/login.html')
-
+@login_required(login_url='/login/')
 def add_event(request):
   if request.method =="POST":
     event_name=request.POST.get("event_name")
@@ -24,7 +41,7 @@ def add_event(request):
 
   return render(request, 'core/add_event.html')
 
-
+@login_required(login_url='/login/')
 def event_description(request,no):
   # events=Event.objects.all()
   event=Event.objects.get(id=no)
